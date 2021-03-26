4f23b43d972ba8dfb8ccf9d7656efb8e9faea418
Jack <jackpmalone01@gmail.comgit config --global user.name jackMalone2001>
2021-03-26 01:34:41 +0000

    After several attempts, i finally got it to work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { OmdbApiService } from './services/omdb-api.service';
+import { IOMDBResponse } from './omdbresponse';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    providers: [OmdbApiService]
 })
+
 export class AppComponent {
-  title = 'Movie-Finder';
+    movieData: IOMDBResponse;
+    errorMessage: any;
+
+    constructor(private _omdbService: OmdbApiService) { }
+
+    getMovieDetails(movieName: string): boolean {
+        this._omdbService.getMovieData(movieName).subscribe(
+            movieData => {
+                this.movieData = movieData;
+                console.log('Director name: ' + this.movieData.Director);
+            },
+            error => this.errorMessage = <any>error
+        );
+
+        return false;
+    }
 }
